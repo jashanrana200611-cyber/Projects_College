a2ff59df6138c2cfebd4d38939c23ad5164076ce
jashanrana200611-cyber <jashanrana200611@gmail.com>
2026-04-26 19:10:28 +0530

    Add files via upload

---
+++ assignemet1_1.py
@@ -0,0 +1,23 @@
+#write a program to take input from 2 numbers and print their sum sub div and multiploation
+
+a = input("Enter the first number :")
+b = input("Enter the second number :")
+
+sum = float(a) + float(b)
+subtract = float(a)-float(b)
+divide = float(a)/float(b)
+multiply = float(a)*float(b)
+
+print("The sum of", a, "and", b, "is:", sum)
+print("The subtraction of", a, "and", b, "is:", subtract)
+# print("The division of", a, "and", b, "is:", divide)
+# hm it is realy easy to comment this 
+# 
+# 
+# 
+# 
+# way
+
+
+print("The multiplication of", a, "and", b, "is:", multiply)
+
